5e4f893232df92c02033a9c33ea5555e566b947e
VivekPatel28 <130638660+VivekPatel28@users.noreply.github.com>
2023-09-24 19:20:36 +1300

    changes to website layout

--- script.js
+++ script.js
@@ -33,3 +33,6 @@ if (exampleModal) {
     modalBodyInput.value = recipient
   })
 }
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
