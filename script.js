fc6c94deab92143ad5501583161c945f4566700a
VivekPatel28 <130638660+VivekPatel28@users.noreply.github.com>
2023-09-27 13:05:45 +1300

    added search functionality

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Get references to the search input and search button
+//   var searchInput = document.getElementById("searchInput");
+//   var searchButton = document.getElementById("searchButton");
+
+//   // Add a click event listener to the search button
+//   searchButton.addEventListener("click", function () {
+//       performSearch();
+//   });
+
+//   // Add a key press event listener to the search input
+//   searchInput.addEventListener("keypress", function (event) {
+//       if (event.key === "Enter") {
+//           performSearch();
+//       }
+//   });
+
+//   // Function to perform the search
+//   function performSearch() {
+//       var searchTerm = searchInput.value.toLowerCase();
+//       // You can replace this with your own logic to search the content on your page
+//       // For example, you can search for keywords in your content and highlight them or display search results.
+//       // Here's a basic example of displaying an alert with the search term:
+//       alert("Searching for: " + searchTerm);
+//   }
+// });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var highlightButton = document.getElementById("searchButton");
+    var searchInput = document.getElementById("searchInput");
+
+    highlightButton.addEventListener("click", function () {
+      highlightWords();
+    });
+
+    searchInput.addEventListener("keyup", function (event) {
+      if (event.key === "Enter") {
+        highlightWords();
+      }
+    });
+
+    function highlightWords() {
+      var searchText = searchInput.value;
+      var paragraphs = document.querySelectorAll("p");
+
+      paragraphs.forEach(function (paragraph) {
+        var paragraphText = paragraph.innerHTML;
+        var highlightedText = paragraphText.replace(new RegExp(searchText, "gi"), function (match) {
+          return '<span class="highlight">' + match + '</span>';
+        });
+
+        paragraph.innerHTML = highlightedText;
+      });
+    }
+  });
+
+
 
 document.addEventListener("DOMContentLoaded", function () {
   const toastTrigger = document.getElementById("liveToastBtn");
