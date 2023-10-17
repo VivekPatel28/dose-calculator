019c1df40bf74a89812d50dcffadbc6963f58888
VivekPatel28 <130638660+VivekPatel28@users.noreply.github.com>
2023-10-17 18:04:55 +1300

    SEO added

--- script.js
+++ script.js
@@ -1,79 +1,83 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle("active");
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(section => {
-        let top = window.scrollY;
-        let offset = section.offsetTop - 150;
-        let height = section.offsetHeight;
-        let id = section.getAttribute('id');
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 150;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-            });
-        };
-    });
-    let header = document.querySelector('header');
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  let header = document.querySelector("header");
 
-    header.classList.toggle('sticky', window.scrollY > 100);
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
 ScrollReveal({
-  reset:true,
-  distance: '80px',
+  reset: true,
+  distance: "80px",
   duration: 2000,
   delay: 200,
+});
 
-})
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
-
-ScrollReveal().reveal('.home-img, .projects-container, .portfolio-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal(
+  ".home-img, .projects-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
 
 toastr.options = {
-    "positionClass": "toast-top-right",
-    "preventDuplicates": true,
-    "showDuration": "300",
-    "hideDuration": "1000",
-    "timeOut": "5000",
-    "extendedTimeOut": "1000",
-    "closeButton": "true"
+  positionClass: "toast-top-right",
+  preventDuplicates: true,
+  showDuration: "300",
+  hideDuration: "1000",
+  timeOut: "5000",
+  extendedTimeOut: "1000",
+  closeButton: "true",
 };
 
-toastr.options.showMethod = 'slideDown';
-toastr.options.hideMethod = 'slideUp';
-toastr.options.closeMethod = 'slideUp';
+toastr.options.showMethod = "slideDown";
+toastr.options.hideMethod = "slideUp";
+toastr.options.closeMethod = "slideUp";
 
 (function () {
-    emailjs.init("GszwseblWsql_Xa53");
+  emailjs.init("GszwseblWsql_Xa53");
 })();
 
-function sendMail(){
-    var params = {
-        from_name: document.getElementById('name').value,
-        from_email: document.getElementById('email').value,
-        message: document.getElementById('message').value,
+function sendMail() {
+  var params = {
+    from_name: document.getElementById("name").value,
+    from_email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs.send("service_g7k0b2p", "template_8akkimr", params).then(
+    function (response) {
+      toastr.success("Email sent successfully!", "Success");
+      document.getElementById("contact-form").reset();
+    },
+    function (error) {
+      toastr.error("Failed to send email. Please try again later.", "Error");
     }
-    emailjs.send('service_g7k0b2p', 'template_8akkimr', params)
-    .then(function(response) {
-    toastr.success('Email sent successfully!', 'Success');
-       document.getElementById('contact-form').reset();
-    }, function(error) {
-        toastr.error('Failed to send email. Please try again later.', 'Error');
-    });
+  );
 }
-
-
