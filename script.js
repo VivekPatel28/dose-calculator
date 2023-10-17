8c614c4222869174e440fafb3f155cb526492466
VivekPatel28 <130638660+VivekPatel28@users.noreply.github.com>
2023-10-17 17:46:25 +1300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onClick = () => {
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('active');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle("active");
 }
-
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -43,10 +42,38 @@ ScrollReveal({
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
 
 ScrollReveal().reveal('.home-img, .projects-container, .portfolio-box, .contact form', { origin: 'bottom'});
-// const typed = new typed('.multiple-text', {
-//     strings: ['Frontend Developer', 'Backend Developer', 'Web Developer'],
-//     typedSpeed: 100,
-//     backSpeed: 100,
-//     backDelay: 1000,
-//     loop: true
-// });
+
+toastr.options = {
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "closeButton": "true"
+};
+
+toastr.options.showMethod = 'slideDown';
+toastr.options.hideMethod = 'slideUp';
+toastr.options.closeMethod = 'slideUp';
+
+(function () {
+    emailjs.init("GszwseblWsql_Xa53");
+})();
+
+function sendMail(){
+    var params = {
+        from_name: document.getElementById('name').value,
+        from_email: document.getElementById('email').value,
+        message: document.getElementById('message').value,
+    }
+    emailjs.send('service_g7k0b2p', 'template_8akkimr', params)
+    .then(function(response) {
+    toastr.success('Email sent successfully!', 'Success');
+       document.getElementById('contact-form').reset();
+    }, function(error) {
+        toastr.error('Failed to send email. Please try again later.', 'Error');
+    });
+}
+
+
